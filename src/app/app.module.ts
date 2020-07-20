1036e99e9604956a515b93abadc3c9d7e0f220b1
vvkravchenko <vitalii.kravchenko.1011@gmail.com>
2020-07-20 20:56:53 +0300

    11 ed hw

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CenzorComponent } from './cenzor/cenzor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CenzorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
